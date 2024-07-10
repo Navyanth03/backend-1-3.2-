7698f72517be533e854711bc34f2b2675c68266b
Navyanth03 <mekalanavyanthyadav2003@gmail.com>
2024-07-10 22:50:44 +0530

    Integrated mongodb for persistant data storage

--- README.md
+++ README.md
@@ -1,13 +1,9 @@
-This is a basic backend application created using express in node js.
-Data is static and will be present in the file itself.
-The function of this back-end is to create and send back a jwt token whenever a user from data logs in (POST)
-and (GET) get the data of other users after authenticating the user.
+## Backend of a basic application
+This applications lets you-
+-Signup 
+-Signin using jwt
+-Providing details of other users
 
-
-
-
-
-
-
+Tech Stack used: Nodejs, express, MongoDB, Mongoose, jwt. 
 
 From Cohort week 3(3.2).
--- myIndex.js
+++ myIndex.js
@@ -1,47 +1,60 @@
 const express=require('express');
 const jwt=require('jsonwebtoken');
+const mongoose=require('mongoose');
+require('dotenv').config();
 const app=express();
-
 const jwtPassword='xyz123';
-const users=[{
-    name: 'Navyanth',
-    username:'navyanth@gmail.com',
-    password:'123456'
-},{
-    name: 'Anurag',
-    username:'anurag@gmail.com',
-    password:'789101112'
-},{
-    name: 'Goutham',
-    username: 'goutham@gmail.com',
-    password: 'abcdef'
-}]
+
+const user=mongoose.model('users',{ name: String, email: String, password: String});
 
 app.use(express.json());
 
-function verifyUser(username,password){
-    return users.find(user=>user.username===username && user.password===password);
-}
-
-app.post('/signin',(req,res)=>{
-    const username=req.body.username;
+app.post('/signup',async(req,res)=>{
+    const name=req.body.name;
+    const email=req.body.email.toLowerCase();
     const password=req.body.password;
-    if(!verifyUser(username,password))return res.status(404).json({msg:'Please provide correct credentials'});
-    const token=jwt.sign({username:username, password: password}, jwtPassword);
-    res.status(200).json({msg: 'you are logged succesfully', token: token});
+    const existingUser=await user.findOne({email:email});
+    if(existingUser){
+        return res.status(400).json({msg:'User exists, please signin'});
+    }
+    const newUser=await user.create({name:name,email:email,password:password});
+    res.status(201).json({msg:'Hurray, you have created an acoount',data:newUser});
 })
 
-app.get('/users',(req,res)=>{
+app.post('/signin',async(req,res)=>{
+    const email=req.body.email;
+    const password=req.body.password;
+    const existingUser=await user.findOne({email:email,password:password});
+    if(!existingUser){
+        return res.status(404).json({msg:'Please provide correct credentials'});
+    }
+    const token=jwt.sign({email:email, password: password}, jwtPassword);
+    res.status(200).json({msg: 'logged in succesfully', token: token});
+})
+
+app.get('/users',async(req,res)=>{
     const token=req.headers.authorization;
     try {
         const response=jwt.verify(token,jwtPassword);
-        const username=response.username;
-        const others= users.filter((user)=>user.username!==username);
+        const email=response.email;
+        let others=await user.find({});
+        //test
+        others=others.filter((data)=>data.email!==email);
         res.status(200).json({msg:'success',data:others});
     } catch (error) {
-        res.status(401).json({msg:'unauthorized access'});
+        res.status(401).json({err:error,msg:'unauthorized access'});
     }
 })
 
 
-app.listen(5000,()=>console.log('Listening on port 5000.....'));
+async function start(){
+    try {
+        await mongoose.connect(process.env.database_Url);
+        console.log('Connected to database...');
+        app.listen(5000,()=>console.log('Listening on port 5000.....')); 
+    } catch (error) {
+        console.log(err);
+    }
+}
+
+start();
